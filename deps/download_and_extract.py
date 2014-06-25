8cfbd3ef4645e5e15243df0c48927fb73f2a94bb
Manu Sporny <msporny@digitalbazaar.com>
2014-06-25 17:17:54 -0400

    Move cefclient.tar.gz location from github to dropbox.

--- deps/download_and_extract.py
+++ deps/download_and_extract.py
@@ -4,7 +4,7 @@ import urllib
 import tarfile
 
 # hardcoded here otherwise gyp on windows will incorrectly try to relativize the path
-source_url = 'https://github.com/downloads/mapbox/cefclient/cefclient-0.0.2.tar.gz'
+source_url = 'https://www.dropbox.com/s/1hyt9q3zac0fbua/cefclient-0.0.2.tar.gz?dl=1'
 
 target_file = os.path.abspath(os.path.basename(source_url))
 dirname = os.path.abspath(sys.argv[1])
