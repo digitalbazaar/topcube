0faa2629390761f60b15f2380f573d619b8db2b1
Manu Sporny <msporny@digitalbazaar.com>
2014-06-25 17:32:50 -0400

    Fix filename to not include '?' because it confuses Windows.

--- deps/download_and_extract.py
+++ deps/download_and_extract.py
@@ -6,7 +6,7 @@ import tarfile
 # hardcoded here otherwise gyp on windows will incorrectly try to relativize the path
 source_url = 'https://www.dropbox.com/s/1hyt9q3zac0fbua/cefclient-0.0.2.tar.gz?dl=1'
 
-target_file = os.path.abspath(os.path.basename(source_url))
+target_file = os.path.abspath('cefclient-0.0.2.tar.gz')
 dirname = os.path.abspath(sys.argv[1])
 
 if not os.path.exists(target_file):
